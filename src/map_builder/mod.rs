e0e20e06c3c8c725e1518efcd4d5accfb6f466de
Russell.Shurts <russell.shurts@target.com>
2024-07-12 13:40:45 -0500

    add map themes

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -2,15 +2,21 @@ mod automata;
 mod drunkard;
 mod prefab;
 mod rooms;
+mod themes;
 
 use crate::map_builder::automata::CelluarAutomataArchitect;
 use crate::map_builder::drunkard::DrunkardsWalkArchitect;
 use crate::map_builder::prefab::apply_prefab;
 use crate::prelude::*;
 use rooms::RoomsArchitect;
+use themes::*;
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
+
+pub trait MapTheme: Sync + Send {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType;
 }
 
 const NUM_ROOMS: usize = 20;
@@ -21,6 +27,7 @@ pub struct MapBuilder {
     pub monster_spawns: Vec<Point>,
     pub player_start: Point,
     pub amulet_start: Point,
+    pub theme: Box<dyn MapTheme>,
 }
 
 impl MapBuilder {
@@ -32,6 +39,12 @@ impl MapBuilder {
         };
         let mut mb = architect.new(rng);
         apply_prefab(&mut mb, rng);
+
+        mb.theme = match rng.range(0, 2) {
+            0 => ForestTheme::new(),
+            _ => DungeonTheme::new(),
+        };
+
         mb
     }
 
